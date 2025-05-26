d1b235100ce66164a9db75a4bbff0fd4e9611118
Bernardo-17 <bernardocandido6780@gmail.com>
2025-05-26 19:41:00 -0300

    Update ex-06.py

--- ex-06.py
+++ ex-06.py
@@ -1,4 +1,6 @@
-#6) Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e escreva a idade dessa pessoa expressa apenas em dias. Considerar ano com 365 dias e mês com 30 dias.
+6) Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e escreva a idade dessa pessoa expressa apenas em dias. Considerar ano com 365 dias e mês com 30 dias.
 anos = int(input('Digite sua idade: '))
 meses = int(input('Digite o mês em que nasceu: '))
-print(f'Sua idade é {(anos * 365)+(meses * 30)}')
+dias = int(input('Digite o dia em que nasceu: '))
+print(f'Você tem {anos} anos, {meses} meses e {dias} dias ')
+print(f'Sua idade em dias é {(anos * 365)+(meses * 30) }')
