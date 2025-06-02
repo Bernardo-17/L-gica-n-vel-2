00746574ed67784d962bc7b00b0922e0184fd7d8
Bernardo-17 <bernardocandido6780@gmail.com>
2025-06-02 20:16:34 -0300

    Update ex-36.py

--- ex-36.py
+++ ex-36.py
@@ -2,24 +2,9 @@
 # (considere que as idades dos homens serão sempre diferentes entre si, bem como as das mulheres). 
 # Calcule e escreva a soma das idades do homem mais velho com a mulher mais 
 # nova, e a soma das idades do homem mais novo com a mulher mais velha.
-tipo_combustivel = input('Digite o combustivel: ' )
-Valor_litragem_gasolina = 2.90
-Valor_litragem_álcool = 3.30
-Valor_desconto_gasolina_menor_20 = 0.04
-Valor_desconto_gasolina_maior_20 = 0.06
-desconto_álcool_menor_20 = 0.03
-desconto_álcool_maior_20 = 0.05
-if tipo_combustivel =='Gasolina':
-    quantidade_combustivel = float(input(f'Digite a quantidade de litros desejados de gasolina: '))
-    if quantidade_combustivel <= 20:
-        print(f'A gasolina fica no valor de: R${quantidade_combustivel * Valor_litragem_gasolina - ((quantidade_combustivel * Valor_litragem_gasolina) * Valor_desconto_gasolina_menor_20):.2f} ')
-    elif quantidade_combustivel > 20:
-        print(f'A gasolina fica no valor de: R${quantidade_combustivel * Valor_litragem_gasolina - ((quantidade_combustivel * Valor_litragem_gasolina)* Valor_desconto_gasolina_maior_20):.2f} ')
-elif tipo_combustivel =='Álcool':
-    quantidade_combustivel = float(input(f'Digite a quantidade de litros desejados de álcool: '))
-    if quantidade_combustivel <= 20:
-        print(f'O álcool fica no valor de: R${quantidade_combustivel * Valor_litragem_álcool - ((quantidade_combustivel * Valor_litragem_álcool) * desconto_álcool_menor_20):.2f} ')
-    elif quantidade_combustivel > 20:
-        print(f'O álcool fica no valor de: R${quantidade_combustivel* Valor_litragem_álcool - ((quantidade_combustivel * Valor_litragem_álcool) * desconto_álcool_maior_20):.2f} ')
-else:
-    print('Algo foi digitado incorretamente.')
+mulherN = int(input('Digite a idade da mulher mais nova: '))
+mulherV = int(input('Digite a idade da mulher mais velha: '))
+homemN = int(input('Digite a idade do homem mais novo: '))
+homemV = int(input('Digite a idade do homem mais velho: '))
+if homemV > homemN and mulherV > mulherN:
+    print(f'A soma das idade do homem mais velho e a mulher mais nova é: {homemV} + {mulherN} = {homemV + mulherN}.\nA soma do homem mais novo com a mulher mais velha é: {homemN} + {mulherV} = {homemN + mulherV}.')
