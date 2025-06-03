9409b4be989c10a744f1d310590b7dc952e05b42
Bernardo-17 <bernardocandido6780@gmail.com>
2025-06-03 20:24:08 -0300

    Create ex-56.py

---
+++ ex-56.py
@@ -0,0 +1,6 @@
+#53) Ler um valor N e imprimir todos os valores inteiros entre 1 (inclusive) e N (inclusive). Considere que o N será sempre maior que ZERO.
+numero = int(input('Digite seu numero'))
+while numero > 0:
+    print(numero)
+    numero -= 1
+   
